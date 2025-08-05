89013178a7f444d96e1fc0a95dbf7d4150c68e2f
Edgar Joel <esanchez1169@uta.edu.ec>
2025-08-05 11:26:57 -0500

    creacion del proyecto con spring boot initializr

---
+++ _persona-cliente-ms/persona-cliente-ms/src/main/java/com/prueba/tecnica/persona_cliente_ms/PersonaClienteMsApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.tecnica.persona_cliente_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaClienteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaClienteMsApplication.class, args);
+	}
+
+}
