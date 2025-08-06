95765ab8d9dd9abce47a5c6dde36ee240665b3d7
Edgar Joel <esanchez1169@uta.edu.ec>
2025-08-06 09:37:29 -0500

    creacion del nuevo micro servicos

---
+++ _persona-cliente-ms/cuenta-movimiento-ms/cuenta-movimiento-ms/src/test/java/com/test/technical/cuenta_movimiento_ms/CuentaMovimientoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.technical.cuenta_movimiento_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaMovimientoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
